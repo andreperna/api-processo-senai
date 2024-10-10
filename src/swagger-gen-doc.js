629c13692865f7e0c9fe87d751b84877d16d24eb
andreperna <andreperna@gmail.com>
2024-10-10 12:19:28 -0300

    update host

--- src/swagger-gen-doc.js
+++ src/swagger-gen-doc.js
@@ -1,34 +1,11 @@
 import swaggerAutogen from 'swagger-autogen';
 
-// const doc = {
-//   info: {
-//     title: 'API - Gestão Escolar',
-//     description: 'API fake para utilização no Processo Seletivo do SENAI'
-//   },
-//   host: 'localhost:3000'
-// };
-
 const doc = {
-  swagger: '2.0',
   info: {
     title: 'API - Gestão Escolar',
-    description: 'API fake para utilização no Processo Seletivo do SENAI',
-    version: '1.0.0',
+    description: 'API fake para utilização no Processo Seletivo do SENAI'
   },
-  host: 'localhost:3000', // Servidor padrão
-  schemes: ['http', 'https'], // Esquema usado (http ou https)
-  paths: {}, // As rotas serão adicionadas automaticamente
-  // Aqui você pode adicionar as definições de servidores adicionais
-  servers: [
-    {
-      url: 'http://localhost:3000', // Primeiro servidor
-      description: 'Servidor local',
-    },
-    {
-      url: 'https://api-processo-senai.onrender.com', // Segundo servidor
-      description: 'Servidor de produção',
-    },
-  ],
+  host: 'api-processo-senai.onrender.com'
 };
 
 const outputFile = './swagger.json';
